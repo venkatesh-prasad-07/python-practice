facfad4cd560358898d76a2b33ce09b9d41d9dbb
Venkatesh Prasad <76904939+venkatesh-prasad-07@users.noreply.github.com>
2021-12-26 12:43:47 +0530

    Create capitalize.py

---
+++ capitalize.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+#capitalizing 1st word for every word in a sentence
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the solve function below.
+def solve(s):
+    for i in s.split():
+        s = s.replace(i,i.capitalize())
+    return s
+            
+ 
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
