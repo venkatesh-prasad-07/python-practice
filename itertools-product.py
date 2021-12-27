2de47ce19a6eaee358428f51e47a429d2dec94dd
Venkatesh Prasad <76904939+venkatesh-prasad-07@users.noreply.github.com>
2021-12-27 09:00:57 +0530

    Create itertools-product.py

---
+++ itertools-product.py
@@ -0,0 +1,10 @@
+from itertools import product
+A_str = input()
+B_str = input()
+A_str = A_str.split()
+A_map = map(int,A_str)
+A = tuple(A_map)
+B_str = B_str.split()
+B_map = map(int,B_str)
+B = tuple(B_map)
+print (list(product(A,B)))
